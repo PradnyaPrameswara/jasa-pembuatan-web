b17c8068e77a76226987c674794ca37306ee317c
Anak Agung Gede Pradnya Prameswara <agungpradnya7@gmail.com>
2026-08-20 15:54:12 +0800

    chore: replace JavaScript config files with TypeScript

--- astro.config.ts
+++ astro.config.ts
@@ -1,16 +1,11 @@
-// @ts-check
+import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
-import react from "@astrojs/react";
-import tailwindcss from "@tailwindcss/vite";
-
-import sitemap from "@astrojs/sitemap";
-
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://production-domain-required.com',
+  site: "https://production-domain-required.com",
   integrations: [react(), sitemap()],
-
   vite: {
     plugins: [tailwindcss()],
   },
--- eslint.config.ts
+++ eslint.config.ts
@@ -1,2 +1,3 @@
 import eslintPluginAstro from "eslint-plugin-astro";
+
 export default [...eslintPluginAstro.configs.recommended];
